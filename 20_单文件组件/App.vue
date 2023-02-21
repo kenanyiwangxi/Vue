523fadcd6422f445f871575c41b45aa754ce79ba
kenanyiwangxi <3215136710@qq.com>
2023-02-21 08:47:35 +0800

    Vue核心完成

---
+++ 20_单文件组件/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+// 引入组件
+import School from "../../vue_test/src/components/School.vue"
+import Student from "../../vue_test/src/components/Student.vue";
+
+export default {
+  name: 'App',
+  components: {
+    School,
+    Student
+  }
+}
+</script>
